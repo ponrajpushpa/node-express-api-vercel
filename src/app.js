4b8aa684cd76510dd18c00ecdc0132ddacd80997
Ponraj C <c.ponraaj@gmail.com>
2026-09-12 13:25:18 +0530

    removed the process exit

--- src/app.js
+++ src/app.js
@@ -60,7 +60,7 @@ app.get('/',(req, res)=>{
 // }
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-  }); 
+}); 
 
 export default app;
 // startServer();
--- src/core/db/index.js
+++ src/core/db/index.js
@@ -7,7 +7,7 @@ const dbConnect = async () => {
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("MongoDB connection error:", error);
-        process.exit(1); // Exit the process with failure
+        // process.exit(1); // Exit the process with failure
     }
 }
 
