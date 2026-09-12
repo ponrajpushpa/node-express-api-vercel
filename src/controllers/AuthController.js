66c862ef31ec4a7c965b8a35a6a0ce7d45b09f49
Ponraj C <c.ponraaj@gmail.com>
2026-09-12 07:06:38 +0530

    change the file name

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import User from "../models/UserModel.js";
 import jwt from "jsonwebtoken";
 import crypto from 'crypto';
 import { 
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import User from "../models/UserModel.js";
 
 const getAllUsers = async (req, res) => {
     const users = await User.find({});
